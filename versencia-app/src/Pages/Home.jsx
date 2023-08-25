cab6093f8eeaf0374a32488ec2dce70192e631f3
shivendra_pratap_dixit <cvendradixit@gmail.com>
2023-08-25 20:26:32 +0530

    day3 login

--- versencia-app/src/Pages/Home.jsx
+++ versencia-app/src/Pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-import { Heading } from '@chakra-ui/react'
+import { Stack,ButtonGroup,Button,Box, } from '@chakra-ui/react'
+import {ArrowForwardIcon} from "@chakra-ui/icons"
+
 const Home = () => {
-  return (
-    <Heading>Versencia</Heading>
-  )
+  return <>
+    <Stack direction='column'>
+  <Box
+    display='flex'
+    alignItems='center'
+    justifyContent='center'
+    // width='100%'
+    height="500px"
+    borderRadius="10px"
+    px={12}
+    bgImage="url('https://wallpaperaccess.com/full/5932245.jpg')"
+    bgPosition='center'
+    bgRepeat='no-repeat'
+    mt={1}
+  >
+    <ButtonGroup gap='5'>
+      <Button colorScheme='whiteAlpha' borderRadius="50px" color="white" backgroundColor="white.500" variant="outline">2023 LookBook</Button>
+      <Button colorScheme='blackAlpha' borderRadius="50px" color="violet" backgroundColor="white" rightIcon={<ArrowForwardIcon />}>Shop Now</Button>
+    </ButtonGroup>
+  </Box>
+</Stack>
+  </>
 }
 
 export default Home
--- versencia-app/src/Pages/Login.jsx
+++ versencia-app/src/Pages/Login.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import {Input,Container,Button,Stack,Heading} from "@chakra-ui/react"
+import { useState } from "react";
+import axios from "axios"
+// import { AuthContext } from "../../context/AuthContextProvider";
+// import { getLogin } from "./api";
+import { useNavigate } from "react-router-dom";
+function Login() {
 
-const Login = () => {
+  const [userData,setData]=useState({
+    email:"",password:""
+  })
+ const navigate =useNavigate()
+ 
+const getLogin=(data)=>{
+    return axios({
+            url:``,
+            method:"post",
+             data:data
+        }).then((res)=>{
+            const token=res?.data?.token
+            return token
+        })
+    
+}
+  // const {login}=useContext(AuthContext)
+  const [loading,setloading]=useState(false)
+  const handleChange=(e)=>{
+const {name,value}=e.target;
+setData({
+  ...userData,[name]:value
+})
+  }
+ const handleSubmit=(e)=>{
+e.preventDefault()
+setloading(true)
+getLogin(userData)
+.then((res)=>{
+  // login(res)
+  setloading(false)
+  navigate(`/`)
+})
+
+ }
+ const {email,password}=userData
   return (
-    <div>Login</div>
-  )
+    <Container maxW="container.md" mt={4}>
+<Heading>Welcome To Versencia</Heading>
+      {/* Add form here */}
+      <Stack>
+    <form onSubmit={handleSubmit}>
+      
+      <Input type ="email" placeholder="Email" name="email" value={email} _placeholder={{ opacity: 1, color: 'gray.600' }} onChange={handleChange}/>
+      <br/>
+      <br/>
+      <Input type="passsword" placeholder="Password" name="password" _placeholder={{ color: 'teal' }} value={password} onChange={handleChange}/>
+      <br/>
+      <br/>
+      <Button type="submit" isLoading={loading}>Login</Button>
+      
+    </form>
+    </Stack>
+    </Container>
+  );
 }
 
-export default Login
+export default Login;
